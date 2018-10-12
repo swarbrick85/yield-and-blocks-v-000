e169919f90eb5b9961c9aa1a5fb7ace5fd877d17
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-12 14:02:40 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,10 +4,10 @@ def hello_t(array)
   while i < array.length 
   yield(array[i])
   i = i + 1 
-  end 
+  end
+  array
   else
   puts "Hey! No block was given!"
-  array
 end
 
 # call your method here!
